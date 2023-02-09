fd64c8eab632dc2102f0b224e9afc096410b6bb5
abdikani <abdikanilibaxa>
2023-02-09 17:35:11 +0300

    update

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,81 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:reastrants_app/constants/constant_color.dart';
 import 'package:reastrants_app/pages/home/bannarPHotos.dart';
 import 'package:reastrants_app/pages/home/smooth_page_indicator.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:websafe_svg/websafe_svg.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
-
+  Home({super.key});
+  final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Column(
-        children: [
-          Container(
-            height: 112,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: darkBlueColor,
-            ),
-            child: Row(
-              children: [
-                Padding(padding: EdgeInsets.only(left: 24)),
-                Expanded(
-                  child: TextField(
-                    onChanged: (value) {},
-                    decoration: InputDecoration(
-                      hintText: "Welcome", hintStyle: GoogleFonts.poppins(
-                        fontSize: 14, fontWeight: FontWeight.w400, color: darkBlueColor
-                      ),
-                      fillColor: lightWhiteColor,
-                      filled: true,
-                      suffixIcon: Icon(Icons.search, weight: 14,),
-                      // suffixIcon: Padding(
-                      //   padding: const EdgeInsets.all(
-                      //       RDefaultPadding * 0.75), //15
-                      //   child: WebsafeSvg.asset(
-                      //     "assets/icons/Search.svg",
-                      //     width: 14,
-                      //   ),
-                      // ),
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(5)),
-                        borderSide: BorderSide.none,
+      body: Material(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 112,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: darkBlueColor,
+              ),
+              child: Row(
+                children: [
+                  Padding(padding: EdgeInsets.only(left: 24)),
+                  Expanded(
+                    child: TextField(
+                      onChanged: (value) {},
+                      decoration: InputDecoration(
+                        hintText: "Welcome", hintStyle: GoogleFonts.poppins(
+                          fontSize: 14, fontWeight: FontWeight.w400, color: darkBlueColor
+                        ),
+                        fillColor: lightWhiteColor,
+                        filled: true,
+                        suffixIcon: Icon(Icons.search, weight: 14,),
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
                     ),
+                    //notification 
                   ),
-                  //notification 
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          //banner images
-         SmoothPage(),
-          // Container(
-          //   height: 161,
-          //   decoration: BoxDecoration(
-
-          //   ),
-          //   child: PageView.builder(
-          //     itemCount: bannarList.length,
-          //     itemBuilder: (context, index){
-          //       return Container(
-          //         decoration: BoxDecoration(
-          //           borderRadius: BorderRadius.circular(20),
-          //           image: DecorationImage(image: NetworkImage(bannarList[index].thumbnailUrl),
-          //           fit: BoxFit.cover
-          //         )),
-          //       );
-          //     }
-          //     ),
-          // )
-        ],
+            //banner images
+           Smoothpage1(),
+           
+           //promotion part 
+           SizedBox(height: 30,),
+           Padding(
+             padding: const EdgeInsets.only(left: 28.0),
+             child: Container(
+              height: 24,
+              width: 116,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: brownColor
+              ),child: Text("Promotion",textAlign: TextAlign.center, style: GoogleFonts.poppins(
+              fontSize: 16, fontWeight: FontWeight.w400, color: lightWhiteColor
+             ),),
+             ),
+           ),
+           
+            
+          ],
+        ),
       ),
     );
   }
--- lib/pages/home/smooth_page_indicator.dart
+++ lib/pages/home/smooth_page_indicator.dart
@@ -4,37 +4,47 @@ import 'package:reastrants_app/pages/home/bannarPHotos.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 
-class SmoothPage extends StatelessWidget {
-  SmoothPage({super.key});
-  final _controller = PageController();
-
+class Smoothpage1 extends StatelessWidget {
+  Smoothpage1({Key? key}) : super(key: key);
+  final _controller = PageController(viewportFraction: 0.7, keepPage: true);
+var index = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 30,
-            child: PageView.builder(
-              itemCount: bannarList.length,
-              itemBuilder: (context, index){
-                return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    image: DecorationImage(image: NetworkImage(bannarList[index].thumbnailUrl),
-                    fit: BoxFit.cover
-                  )),
-                );
-              }
+    return Column(
+      children: [
+        Container(
+              margin: EdgeInsets.only(top: 18),
+              height: 161,
+              width: 340,
+              decoration: BoxDecoration(
+      
               ),
-          ),
-          SmoothPageIndicator(controller: _controller, 
-          count: 4,
-          
-          )
-        ],
-      ),
+              child: PageView.builder(
+                controller: _controller,
+                itemCount: bannarList.length,
+                itemBuilder: (_, index){
+                  return Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(image: NetworkImage(bannarList[index].thumbnailUrl),
+                      fit: BoxFit.cover
+                    )),
+                  );
+                }
+                ),
+            ),
+            SmoothPageIndicator(
+              controller: _controller, 
+              count: bannarList.length,
+              effect: JumpingDotEffect(
+                dotHeight: 16,
+                dotWidth: 16,
+                jumpScale: .7,
+                verticalOffset: 15,
+              ),
+            )
+      ],
     );
   }
-  
 }
+
